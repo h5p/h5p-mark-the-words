8fe8f5964186d2d13e3a87dfaed484b3e6d1907e
Thomas Horn Sivertsen <thomass@cerpus.com>
2018-03-27 16:20:35 +0200

    Added/modified calculation of max score

--- presave.js
+++ presave.js
@@ -1,18 +1,21 @@
 var H5PPresave = H5PPresave || {};
 
 H5PPresave['H5P.MarkTheWords'] = function (content, finished) {
-    var score = 0;
-    if( content.hasOwnProperty('textField') === true ){
-        var pattern = /\*[^\*\s]+\*/g;
-        score = content.textField.match(pattern || []).length;
-    }
+  var presave = H5PEditor.Presave;
+  var score = 0;
 
-    if( isNaN(score) || score < 0){
-        throw {
-            name: 'InvalidMaxScore Error',
-            message: "Could not calculate the max score for this content. The max score is assumed to be 0. Contact your administrator if this isn’t correct."
-        };
-    }
+  if (isContentValid()) {
+    var pattern = /\*[^\*\s]+\*/g;
+    score = content.textField.match(pattern || []).length;
+  }
 
+  presave.validateScore(score);
+
+  if (finished) {
     finished({maxScore: score});
+  }
+
+  function isContentValid() {
+    return presave.checkNestedRequirements(content, 'content.textField');
+  }
 };
