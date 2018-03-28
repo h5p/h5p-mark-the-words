fb839ef404b4fb1d80da9db485edce638567033c
Thomas Horn Sivertsen <thomass@cerpus.com>
2018-03-28 11:44:54 +0200

    Added JSDoc

--- presave.js
+++ presave.js
@@ -1,5 +1,12 @@
 var H5PPresave = H5PPresave || {};
 
+/**
+ * Resolve the presave logic for the content type Mark the Words
+ *
+ * @param {object} content
+ * @param finished
+ * @constructor
+ */
 H5PPresave['H5P.MarkTheWords'] = function (content, finished) {
   var presave = H5PEditor.Presave;
   var score = 0;
@@ -15,6 +22,10 @@ H5PPresave['H5P.MarkTheWords'] = function (content, finished) {
     finished({maxScore: score});
   }
 
+  /**
+   * Check if required parameters is present
+   * @return {boolean}
+   */
   function isContentValid() {
     return presave.checkNestedRequirements(content, 'content.textField');
   }
